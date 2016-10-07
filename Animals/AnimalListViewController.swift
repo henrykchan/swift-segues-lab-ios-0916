bae5a896ff68968990f47a91c6578f8a8268c4e4
henrykchan <chan.henryk@gmail.com>
2016-10-07 10:48:46 -0400

    seguesLab Complete

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,54 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    
+    
+    var arrayOfAnimals = [ "🐶", "🐱", "🐭", "🐹", "🐰", "🐼", "🦁", "🐷", "🐸", "🐙"]
+    
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arrayOfAnimals.count
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        
+        let animalRow = arrayOfAnimals[indexPath.row]
+        
+        cell.textLabel?.text = animalRow
+        
+        return cell
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "AnimalDetailView" {
+            
+            let dest = segue.destination as! AnimalViewController
+            
+            let pickedRow = tableView.indexPathForSelectedRow?.row
+            
+            if let unwrappedRow = pickedRow {
+                
+                dest.animal = arrayOfAnimals[unwrappedRow]
+            }
+        }
+}
+
+
+
+
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,56 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    
+    @IBOutlet weak var pictureLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    
+    var animal :String?
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+       
+        if let animal = animal {
+            pictureLabel?.text = animal
+            
+            let theAnimal = animalToName(animal)
+            wordLabel?.text = theAnimal
+            navigationItem.title = theAnimal
+            
+        }
+    }
+    
+    
+    fileprivate func animalToName(_ emoji: String) -> String {
+        
+        switch emoji {
+        case "🐶":
+            return "Dog"
+        case "🐱":
+            return "Cat"
+        case "🐭":
+            return "Mouse"
+        case "🐹":
+            return "Hamster"
+        case "🐰":
+            return "Bunny"
+        case "🐼":
+            return "Panda"
+        case "🦁":
+            return "Lion"
+        case "🐷":
+            return "Pig"
+        case "🐸":
+            return "Frog"
+        case "🐙":
+            return "Octupus"
+        default:
+            return ""
+        }
+    }
 }
